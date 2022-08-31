650caf751d2ca030d32d3a074255e81662032a20
Jesse Brooks <brooks.jesse83@gmail.com>
2022-08-31 01:11:16 -0700

    added homepage component

---
+++ react-app/src/components/LoginPage/index.js
@@ -0,0 +1,14 @@
+import './LoginPage.css'
+
+function HomePage() {
+    return (
+        <>
+        <div class='login-container'></div>
+            <div>
+                
+            </div>
+        </>
+    )
+}
+
+export default HomePage;
