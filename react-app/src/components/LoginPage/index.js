bbbc84b51bcc3bd34df56cfc1d6001f1bd17260c
Jesse Brooks <brooks.jesse83@gmail.com>
2022-08-31 20:20:47 -0700

    added css and html to login form component

--- react-app/src/components/LoginPage/index.js
+++ react-app/src/components/LoginPage/index.js
@@ -3,10 +3,19 @@ import './LoginPage.css'
 function HomePage() {
     return (
         <>
-        <div class='login-container'></div>
-            <div>
-                
-            </div>
+        <div class='login-container'>
+            <form>
+                <h1>
+                    Sign in
+                </h1>
+                <label for='email' class='form-label'>Email</label>
+                <input type='email' id='email'></input>
+                <input type='submit'>Continue</input>
+                <div class='login-divider'>
+                    New to Nozama?
+                </div>
+            </form>
+        </div>
         </>
     )
 }
